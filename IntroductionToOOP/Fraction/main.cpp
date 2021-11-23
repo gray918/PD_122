bf906726c0943a27642440ead0aa22c3ed03818a
Clayman <Clayman@DESKTOP-DM8E77U>
2021-11-23 13:47:34 +0200

    CompoundAssignments DONE.

--- IntroductionToOOP/Fraction/main.cpp
+++ IntroductionToOOP/Fraction/main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 
+class Fraction;
+Fraction operator*(Fraction left, Fraction right);	//Прототип оператора *
+//Прототип функции - это объявление функции (Function declaration)
+
 class Fraction
 {
 	int integer;	//Целая часть
@@ -61,9 +65,43 @@ public:
 		set_denominator(denominator);
 		cout << "Constructor:\t" << this << endl;
 	}
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
 	~Fraction()
 	{
 		cout << "Destructor:\t" << this << endl;
+	}
+
+	//					Operators:
+	Fraction& operator=(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	Fraction& operator*=(const Fraction& other)
+	{
+		return *this = *this*other;
+	}
+
+	//					Increment/Decrement
+	Fraction& operator++()
+	{
+		integer++;
+		return *this;
+	}
+	Fraction operator++(int)
+	{
+		Fraction old = *this;
+		integer++;
+		return old;
 	}
 
 	//					Methods:
@@ -125,7 +163,62 @@ Fraction operator/(const Fraction& left, const Fraction& right)
 	return left * right.inverted();
 }
 
+ostream& operator<<(ostream& os, const Fraction& obj)
+{
+	if (obj.get_integer())os << obj.get_integer();
+	if (obj.get_numerator())
+	{
+		if (obj.get_integer())os << "(";
+		os << obj.get_numerator() << "/" << obj.get_denominator();
+		if (obj.get_integer())os << ")";
+	}
+	else if (obj.get_integer() == 0) os<< 0;
+	return os;
+}
+istream& operator>>(istream& is, Fraction& obj)
+{
+	/*int integer;
+	int numerator;
+	int denominator;
+	is >> integer >> numerator >> denominator;
+	obj.set_integer(integer);
+	obj.set_numerator(numerator);
+	obj.set_denominator(denominator);*/
+	const int SIZE = 80;
+	char buffer[SIZE] = {};
+	//is >> buffer;
+	is.getline(buffer, SIZE);
+	//5
+	//1/2
+	//2(3/4)
+	//2 3/4
+	char delimiters[] = "() /";	//Разделители, по которым мы будем делить строку
+	char* number[5];	//В этом массиве буду храниться указатели на числа в buffer
+	int n = 0;	//Счетчик прочитанных из строки чисел
+	//http://cplusplus.com/reference/cstring/
+	//http://cplusplus.com/reference/cstring/strtok/
+	for (char* pch = strtok(buffer, delimiters); pch; pch = strtok(NULL, delimiters))
+	{
+		number[n++] = pch;
+	}
+	switch (n)
+	{
+		//int atoi(char* str) - преобразует строку в целое число
+	case 1:obj.set_integer(atoi(number[0])); break;
+	case 2:
+		obj.set_numerator(atoi(number[0]));
+		obj.set_denominator(atoi(number[1]));
+		break;
+	case 3:
+		obj.set_integer(atoi(number[0]));
+		obj.set_numerator(atoi(number[1]));
+		obj.set_denominator(atoi(number[2]));
+	}
+	return is;
+}
+
 //#define CONSTRUCTORS_CHECK
+//#define ARITHMETICAL_OPERATORS_CHECK
 
 void main()
 {
@@ -146,15 +239,30 @@ void main()
 	D.print();
 #endif // CONSTRUCTORS_CHECK
 
-	int a = 2;
-	int b = 3;
-	int c = a * b;
+#ifdef ARITHMETICAL_OPERATORS_CHECK
+	double a = 2;
+	double b = 3;
+	double c = a * b;
+	c++;
 
 	Fraction A(2, 3, 4);
 	Fraction B(3, 4, 5);
-	Fraction C = A * B;
+	/*Fraction C = A * B;
 	C.print();
 
+
 	C = A / B;
-	C.print();
+	C.print();*/
+
+	A *= B;
+	A = B++;
+	/*A.print();
+	B.print();*/
+	cout << A << endl;
+	cout << B << endl;
+#endif // ARITHMETICAL_OPERATORS_CHECK
+
+	Fraction A;
+	cout << "Введите простую дробь: "; cin >> A;
+	cout << A << endl;
 }
